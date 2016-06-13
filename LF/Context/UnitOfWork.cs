711bde02fa65499a0d420c6ec650bf5b2f3ea7b4
StoyanPetkov <stoyanpetkov91@gmail.com>
2016-06-13 21:36:23 +0300

    Login, Create account, Login with Google+, Reset password, Forgotten password.

---
+++ LF/Context/UnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LF.Context
+{
+    public class UnitOfWork : IDisposable
+    {
+        //private LFContext context = new LFContext();
+
+        //private DbContextTransaction transaction = null;
+
+        //public DbContext Context { get; private set; }
+
+        //public UnitOfWork()
+        //{
+        //    this.transaction = context.Database.BeginTransaction();
+        //    this.Context = context;
+        //}
+
+        //public void Commit()
+        //{
+        //    if (this.transaction != null)
+        //    {
+        //        this.transaction.Commit();
+        //        this.transaction = null;
+        //    }
+        //}
+
+        //public void RollBack()
+        //{
+        //    if (this.transaction != null)
+        //    {
+        //        this.transaction.Rollback();
+        //        this.transaction = null;
+        //    }
+        //}
+
+        public void Dispose()
+        {
+            //Commit();
+            //context.Dispose();
+        }
+    }
+}
---
+++ LF/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LF.Startup))]
+namespace LF
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
